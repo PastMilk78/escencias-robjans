166a9a3f4a8ddae28971ac3e7dcba6012ccdcebd
kiwilimonn <barba_salvador@hotmail.com>
2025-03-29 11:08:31 -0600

    Simplifica la configuración de Next.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Excluye el directorio de scripts de la compilación
-  webpack: (config, { isServer }) => {
-    // Excluir scripts del proceso de construcción
-    config.externals = config.externals || [];
-    if (isServer) {
-      config.externals.push('src/scripts/**/*');
-    }
-    return config;
-  }
-};
+const nextConfig = {};
 
 module.exports = nextConfig; 
