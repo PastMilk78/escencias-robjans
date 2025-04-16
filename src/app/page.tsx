ebcb530d6dbca1fb85d91c43c0c3d6bf21b2ec5d
PastMilk78 <pastillasdeafrecho78@gmail.com>
2025-04-15 19:35:55 -0600

    Update store address to Dolores Hidalgo location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -286,7 +286,7 @@ export default async function Home() {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                       </svg>
                     </div>
-                    <span className="text-lg">Calle Querétaro #20 Colonia Centro</span>
+                    <span className="text-lg">Querétaro SN20, Centro, 37800 Dolores Hidalgo Cuna de la Independencia Nacional, Gto.</span>
                   </li>
                   <li className="flex items-center font-raleway">
                     <div className="bg-[#fed856] p-3 rounded-full mr-4">
@@ -317,7 +317,7 @@ export default async function Home() {
             <div className="md:w-1/2">
               <div className="rounded-lg overflow-hidden h-96 border-2 border-[#fed856] shadow-xl transform transition-transform hover:scale-105">
                 <iframe 
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.073536700303!2d-100.81736792426984!3d20.32142951246767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842ad409c9b66aed%3A0x2a6afd82a06ae63d!2sQuer%C3%A9taro%2020%2C%20Centro%2C%20Celaya%2C%20Gto.!5e0!3m2!1ses-419!2smx!4v1713916148935!5m2!1ses-419!2smx" 
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.9752990865485!2d-100.93652559678213!3d21.15805388400936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842b3f071b6a424f%3A0x822f90a5ec4451f4!2sQuer%C3%A9taro%20SN%2C%20Centro%2C%2037800%20Dolores%20Hidalgo%20Cuna%20de%20la%20Independencia%20Nacional%2C%20Gto.!5e0!3m2!1ses-419!2smx!4v1714182071842!5m2!1ses-419!2smx" 
                   width="100%" 
                   height="100%" 
                   style={{ border: 0 }} 
